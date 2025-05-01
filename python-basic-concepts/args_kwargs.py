16a13268f12a1a9c916e5eb67ade2c15648bf219
Sukanya Ganguwar <33933234+sukanyaganguwar@users.noreply.github.com>
2025-05-01 18:17:06 +0530

    Create args_kwargs.py

---
+++ python-basic-concepts/args_kwargs.py
@@ -0,0 +1,31 @@
+def shipping(*args,**kwargs):
+    print(type(args))      # This is of type tuple
+    print(type(kwargs))    # This is of type dict
+
+    for arg in args:
+        print(arg)
+    print()
+
+
+    for key, value in kwargs.items():    
+      print(f"{key} : {value}")
+
+    for key in kwargs.keys():
+       print(f"{key}")    
+
+    for value in kwargs.values():
+       print(f"{value}")   
+
+    if kwargs.get("fname") == "John":
+       print("Key is Present")
+
+
+shipping("abr", "adb", "dce", fname="John",lname="Brad")    
+
+
+# Notes on functions:
+# *args --> allows you to pass multiple on-key arguments.
+# **Kwargs --> allows you to pass multiple keyword-arguments.
+
+#  * --> here astriek is considered as unpacking operator
+# Types of function parameters  1. positional 2. default 3.Keyword 4.Arbitrary 
