a0e0d64ab27e75323b7e0d3993ec66f9ad6ee9cd
Sukanya Ganguwar <33933234+sukanyaganguwar@users.noreply.github.com>
2025-05-03 14:13:25 +0530

    updated if-else.py

--- control-flow/if-else.py
+++ control-flow/if-else.py
@@ -1,4 +1,6 @@
+# if else condition examples:
 
+# example1
 num = 0
 
 if num <= 5:
@@ -7,3 +9,19 @@ elif num == 0:
     print("zero 0")
 else:
     print("greater than 5")
+
+
+
+# example 2
+age = int(input("Enter your age :"))
+price = 10.00
+
+if age > 65:
+    print(" You are a Senior Citizen")
+    print(f" You are a Senior citizen , you need to pay {price * 0.75}") 
+elif age > 18:
+    print("You are a Adult")
+    print(f" You are a Adult , you need to pay {price}") 
+else:
+    print("You are a Child")
+    print(f" You are a child , you need to pay {price * 0.5}")        
