f5ab8a7887c0720dd07af0fec09a6e7983b0cb3a
Sukanya Ganguwar <33933234+sukanyaganguwar@users.noreply.github.com>
2025-05-03 14:27:06 +0530

    Update output_formatting.py

--- python-basic-concepts/output_formatting.py
+++ python-basic-concepts/output_formatting.py
@@ -9,3 +9,42 @@ print("The value of a is {1} and value of b is {0} ".format(a,b)) # this is call
 name = " Sukanya";
 greetings = "Have a nice day";
 print("Hello,{n} and {g} ".format(n=name,g=greetings)) # this is called specific output formatting
+
+
+# MORE WAYS TO PRINT STATEMENTS IN PYTHON
+print("I Like Pizza")
+
+full_name= "Nancy"
+
+print(f" Hello {full_name}")
+
+age = 25
+gpa= 2.3
+is_true = True
+
+print(f" you are {age} years old")
+print(f" you got this gpa {gpa}")
+print(f" Are you a student ? {is_true}") 
+
+#there are more ways to print a statement with variable.
+
+fave_language = "Python"
+
+print("I like coding in" + " " + fave_language + " "  + "the most")
+
+#output
+#I like coding in Python the most
+
+name = "John"
+
+print("Hello",name)
+
+#output
+#Hello John
+
+first_name = "John"
+
+print("Hello",first_name,"good to see you")
+
+#output
+#Hello John good to see you
