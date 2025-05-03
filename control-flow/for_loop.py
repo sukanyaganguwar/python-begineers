b267cb9aff693d6e752b5b41426d9f4535fa8698
Sukanya Ganguwar <33933234+sukanyaganguwar@users.noreply.github.com>
2025-05-03 14:10:38 +0530

    added for_loop.py

---
+++ control-flow/for_loop.py
@@ -0,0 +1,47 @@
+# Example for Loops:
+
+for i in range(10):
+    print(i)
+
+print("")
+
+for i in range(1,11):
+    print(i)
+
+print()    
+
+name="Bro Code"
+
+for letters in name:
+    print(letters)    
+
+print("")
+
+for letters in name:
+    print(letters, end=" ")
+
+print("")
+for letters in name:
+    print(letters, end="-")        
+
+print("")   
+
+
+for i in range(1,11,2):
+    print(i)
+
+print()
+
+
+for i in range(1,11,3):
+    print(i)
+
+print()
+
+import time
+
+for i in range(10,0,-1):
+    print(i)
+    time.sleep(1)
+
+
