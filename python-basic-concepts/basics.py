fc4617ffe64e412caffa44536135526bb77ce658
Sukanya Ganguwar <33933234+sukanyaganguwar@users.noreply.github.com>
2025-05-03 14:25:11 +0530

    Create basics.py

---
+++ python-basic-concepts/basics.py
@@ -0,0 +1,17 @@
+# learn how to print statements in python
+print("Hello World")
+
+# One of the main benefits of Python is its ability to operate dynamically.
+# This includes the ability to inspect, modify, and even generate many aspects of a program at runtime.
+
+
+#how to run any python program
+# python Program1.py
+# python3 hello.py
+
+
+help(print)  #help keyword , helps us to know the details about the keyword we want to know about.
+
+print(type(print))
+
+help(type(print)) # helps us to know if this is a built in function
